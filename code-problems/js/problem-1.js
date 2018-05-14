963f34491b1bd5c645a8615390fee14b3d5b7e7b
Justin Chang <justin.p.chang@gmail.com>
2018-05-14 16:42:52 -0400

    Add better formatting for problem 1

---
+++ code-problems/js/problem-1.js
@@ -0,0 +1,10 @@
+/*! Problem 1 solution (JavaScript)
+ *  Justin Chang
+ *  15 May 2018
+ * */
+
+$(document).ready(function() {
+  $("#submit-string-btn").click(function() {
+    $("#substring-output").text($("#submit-string").val());
+  });
+});
---
+++ code-problems/js/problem-2.js
@@ -0,0 +1,8 @@
+/*! Problem 2 solution (JavaScript)
+ *  Justin Chang
+ *  15 May 2018
+ * */
+
+$(document).ready(function() {
+
+});
