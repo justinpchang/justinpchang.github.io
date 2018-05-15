2b33ea22016ef424c22ebb1bfdc3eddd03a7740b
Justin Chang <justin.p.chang@gmail.com>
2018-05-14 21:24:45 -0400

    Add problem 2 functionality

--- code-problems/js/problem-2.js
+++ code-problems/js/problem-2.js
@@ -2,7 +2,36 @@
  *  Justin Chang
  *  15 May 2018
  * */
-
+var msg,key;
 $(document).ready(function() {
+  var res = "";
+  msg = "";
+  var count = 0;
+  keys = {};
+  var alphabet_decoded = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,;:!?\'";
 
+  // Create new WebSocket
+  var socket = new WebSocket("ws://206.189.172.118");
+
+  // Create listener to grab responses
+  socket.addEventListener('message', function (event) {
+    res = event.data;
+    // Get first message then subsequent 26 letter messages
+    if(count === 0) {
+      // Get first encoded message from the server
+      msg = res.split(" ");
+      // Send alphabet to be encoded
+      socket.send(alphabet_decoded);
+      count++;
+    } else if(count === 1) {
+      // Get encoded alphabet
+      var alphabet_encoded = res.split(" ");
+      // Decode message using received alphabet
+      var i = alphabet_decoded.length;
+      while(i--) {
+        msg.forEach(function(letter, idx) { if(letter == alphabet_encoded[i]) msg[idx] = alphabet_decoded[i]; });
+      }
+      msg = msg.join("");
+    };
+  });
 });
